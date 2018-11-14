6ddaf8a2b59430ddbfc1279295d44569884be9d7
nanogens <37455949+nanogens@users.noreply.github.com>
2018-11-13 19:59:36 -0500

    Updating
    Nothing more to say..

--- 3_1_HelloWorld/main.cpp
+++ 3_1_HelloWorld/main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
-
+#include <QtDebug>
+#include <iostream>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug("Hello Qt World!");
+    qDebug("Hello Qt World! My %d application.", 1);
+
+    // with #include <QtDebug>
+    qDebug() << "Hello Qt World!" << "My" << 2 << "application.";
+    int num = 1;
+    std::cout << "Hello Qt World" << num << std::endl;
     return a.exec();
 }
